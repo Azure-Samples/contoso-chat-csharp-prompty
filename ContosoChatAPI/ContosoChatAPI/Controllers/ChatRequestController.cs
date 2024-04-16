139e3e308be94e94862e4b76523eb84694c3ea74
Cassie Breviu <46505951+cassiebreviu@users.noreply.github.com>
2024-04-16 10:29:35 -0500

    signing all commits

--- ContosoChatAPI/ContosoChatAPI/Controllers/ChatRequestController.cs
+++ ContosoChatAPI/ContosoChatAPI/Controllers/ChatRequestController.cs
@@ -8,26 +8,26 @@ using System.IO;
 
 namespace ContosoChatAPI.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ContosoChatController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class ContosoChatController : ControllerBase
+  {
+
+    private readonly ILogger<ContosoChatController> _logger;
+
+    public ContosoChatController(ILogger<ContosoChatController> logger)
     {
-
-        private readonly ILogger<ContosoChatController> _logger;
-
-        public ContosoChatController(ILogger<ContosoChatController> logger)
-        {
-            _logger = logger;
-        }
-
-
-        [HttpPost(Name = "PostChatRequest")]
-        public string Post(string customerId, string question, List<string> chatHistory)
-        {
-            var chatService = new ChatService();
-            string result = chatService.GetResponseAsync(customerId, question, chatHistory.ToList()).Result;
-            Console.WriteLine(result);
-            return result;
-        }
+      _logger = logger;
     }
+
+
+    [HttpPost(Name = "PostChatRequest")]
+    public string Post(string customerId, string question, List<string> chatHistory)
+    {
+      var chatService = new ChatService();
+      string result = chatService.GetResponseAsync(customerId, question, chatHistory.ToList()).Result;
+      Console.WriteLine(result);
+      return result;
+    }
+  }
 }
