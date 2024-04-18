d75d68c8ed5e37e72e29b0d4fd4c82b845fd58f0
Cassie Breviu <46505951+cassiebreviu@users.noreply.github.com>
2024-04-18 12:04:11 -0500

    Add evaluations

--- ContosoChatAPI/ContosoChatAPI/Services/ChatService.cs
+++ ContosoChatAPI/ContosoChatAPI/Services/ChatService.cs
@@ -7,113 +7,89 @@ using ContosoChatAPI.Evaluations;
 
 namespace ContosoChatAPI.Services
 {
-    public class ChatService
+  public class ChatService
+  {
+    private readonly IConfiguration _consmos;
+    private readonly string _cosmosEndpoint;
+    private readonly string _cosmosKey;
+
+    private readonly IConfiguration _prompty;
+    private readonly string _oaiEndpoint;
+    private readonly string _oaiKey;
+
+    private readonly IConfiguration _aiSearch;
+    private readonly string _aiSearchEndpoint;
+    private readonly string _aiSearchKey;
+
+    public ChatService()
     {
-        private readonly IConfiguration _consmos;
-        private readonly string _cosmosEndpoint;
-        private readonly string _cosmosKey;
+      // get endpoint and key from appsettings.json
+      var config = new ConfigurationBuilder()
+          .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+          .AddJsonFile("appsettings.json").Build();
 
-        private readonly IConfiguration _prompty;
-        private readonly string _oaiEndpoint;
-        private readonly string _oaiKey;
+      _consmos = config.GetSection("CosmosDB");
+      _prompty = config.GetSection("prompty");
+      _aiSearch = config.GetSection("AzureAISearch");
 
-        private readonly IConfiguration _aiSearch;
-        private readonly string _aiSearchEndpoint;
-        private readonly string _aiSearchKey;
+      _cosmosEndpoint = _consmos["Endpoint"];
+      _cosmosKey = _consmos["Key"];
 
-        public ChatService() {
-            // get endpoint and key from appsettings.json
-            var config = new ConfigurationBuilder()
-                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                .AddJsonFile("appsettings.json").Build();
+      _oaiEndpoint = _prompty["azure_endpoint"];
+      _oaiKey = _prompty["api_key"];
 
-            _consmos = config.GetSection("CosmosDB");
-            _prompty = config.GetSection("prompty");
-            _aiSearch = config.GetSection("AzureAISearch");
+      _aiSearchEndpoint = _aiSearch["Endpoint"];
+      _aiSearchKey = _aiSearch["Key"];
+    }
+    public async Task<string> GetResponseAsync(string customerId, string question, List<string> chatHistory)
+    {
 
-            _cosmosEndpoint = _consmos["Endpoint"];
-            _cosmosKey = _consmos["Key"];
-            
-            _oaiEndpoint = _prompty["azure_endpoint"];
-            _oaiKey = _prompty["api_key"];
+      Console.WriteLine($"Inputs: CustomerId = {customerId}, Question = {question}");
 
-            _aiSearchEndpoint = _aiSearch["Endpoint"];
-            _aiSearchKey = _aiSearch["Key"];
-        }
-        public async Task<string> GetResponseAsync(string customerId, string question, List<string> chatHistory)
-        {
-            
-            Console.WriteLine($"Inputs: CustomerId = {customerId}, Question = {question}");
+      // get customer data
+      var customerData = new CustomerData(_cosmosEndpoint, _cosmosKey);
+      var customer = await customerData.GetCustomerAsync(customerId);
+      // get question embedding
 
-            // get customer data
-            var customerData = new CustomerData(_cosmosEndpoint, _cosmosKey);
-            var customer = await customerData.GetCustomerAsync(customerId);
-            // get question embedding
+      var embeddingData = new EmbeddingData(_oaiEndpoint, _oaiKey);
+      var embedding = embeddingData.GetEmbedding(question);
 
-            var embeddingData = new EmbeddingData(_oaiEndpoint, _oaiKey);
-            var embedding = embeddingData.GetEmbedding(question);
+      var aiSearch = new AISearchData(_aiSearchEndpoint, _aiSearchKey, "contoso-products");
+      var context = await aiSearch.RetrieveDocumentationAsync(question, embedding);
 
-            var aiSearch = new AISearchData(_aiSearchEndpoint, _aiSearchKey, "contoso-products");
-            var context = await aiSearch.RetrieveDocumentationAsync(question, embedding);
-
-            var inputs = new Dictionary<string, dynamic>
+      var inputs = new Dictionary<string, dynamic>
             {
                 { "customer", customer },
-<<<<<<< HEAD
-<<<<<<< HEAD
                 { "documentation", context },
                 { "question", question },
-=======
-                { "context", context },
-                { "embedding", embedding },
->>>>>>> af1c39a (contoso chat logic added)
-=======
-                { "documentation", context },
-                { "question", question },
->>>>>>> cb92d26 (fix input mapping)
                 { "chatHistory", chatHistory }
             };
-            // load chat.json file as new dictionary<string, string>
-            //var jsonInputs = System.IO.File.ReadAllText("chat.json");
-            // convert json to dictionary<string, string>
-            //var inputs = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonInputs);
+      // load chat.json file as new dictionary<string, string>
+      //var jsonInputs = System.IO.File.ReadAllText("chat.json");
+      // convert json to dictionary<string, string>
+      //var inputs = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonInputs);
 
-            var prompty = new Prompty.Core.Prompty();
-            prompty.Inputs = inputs;
+      var prompty = new Prompty.Core.Prompty();
+      prompty.Inputs = inputs;
 
-            Console.WriteLine("Getting result...");
-            prompty = await prompty.Execute("chat.prompty", prompty);
-<<<<<<< HEAD
-            var result = prompty.ChatResponseMessage.Content;
+      Console.WriteLine("Getting result...");
+      prompty = await prompty.Execute("chat.prompty", prompty);
+      var result = prompty.ChatResponseMessage.Content;
 
-            // Create score dict with results
-            var score = new Dictionary<string, string>();
+      // Create score dict with results
+      var score = new Dictionary<string, string>();
 
-            score["groundedness"] = await Evaluation.Evaluate(question, context, result, "./Evaluations/groundedness.prompty");
-            score["coherence"] = await Evaluation.Evaluate(question, context, result, "./Evaluations/coherence.prompty");
-            score["relevance"] = await Evaluation.Evaluate(question, context, result, "./Evaluations/relevance.prompty");
-            score["fluency"] = await Evaluation.Evaluate(question, context, result, "./Evaluations/fluency.prompty");
+      score["groundedness"] = await Evaluation.Evaluate(question, context, result, "./Evaluations/groundedness.prompty");
+      score["coherence"] = await Evaluation.Evaluate(question, context, result, "./Evaluations/coherence.prompty");
+      score["relevance"] = await Evaluation.Evaluate(question, context, result, "./Evaluations/relevance.prompty");
+      score["fluency"] = await Evaluation.Evaluate(question, context, result, "./Evaluations/fluency.prompty");
 
-            Console.WriteLine($"Result: {result}");
-            Console.WriteLine($"Score: {string.Join(", ", score)}");
-            // add score to result
-            result = JsonConvert.SerializeObject(new { result, score });
+      Console.WriteLine($"Result: {result}");
+      Console.WriteLine($"Score: {string.Join(", ", score)}");
+      // add score to result
+      result = JsonConvert.SerializeObject(new { result, score });
 
-            return result;
-=======
-
-            // not implemented yet
-            //var score = new Dictionary<string, double>();
-            //score["groundedness"] = EvaluateGroundedness(question, context, result);
-            //score["coherence"] = EvaluateCoherence(question, context, result);
-            //score["relevance"] = EvaluateRelevance(question, context, result);
-            //score["fluency"] = EvaluateFluency(question, context, result);
-
-            //Console.WriteLine($"Result: {result}");
-            //Console.WriteLine($"Score: {string.Join(", ", score)}");
-
-            return prompty.ChatResponseMessage.Content;
->>>>>>> af1c39a (contoso chat logic added)
-        }
+      return result;
     }
+  }
 }
