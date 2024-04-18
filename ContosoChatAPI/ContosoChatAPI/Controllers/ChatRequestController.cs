970b9a0d993ce86c36d86d2f675fc27041720b54
Cassie Breviu <46505951+cassiebreviu@users.noreply.github.com>
2024-04-17 20:07:24 -0500

    chat updates with static chat.json data

--- ContosoChatAPI/ContosoChatAPI/Controllers/ChatRequestController.cs
+++ ContosoChatAPI/ContosoChatAPI/Controllers/ChatRequestController.cs
@@ -17,7 +17,43 @@ namespace ContosoChatAPI.Controllers
 
     public ContosoChatController(ILogger<ContosoChatController> logger)
     {
+<<<<<<< HEAD
       _logger = logger;
+=======
+
+        private readonly ILogger<ContosoChatController> _logger;
+
+        public ContosoChatController(ILogger<ContosoChatController> logger)
+        {
+            _logger = logger;
+        }
+
+        public static async Task<string> RunPrompt(Dictionary<string, dynamic> inputs)
+        {
+            var prompty = new Prompty.Core.Prompty();
+            prompty.Inputs = inputs;
+            prompty = await prompty.Execute("chat.prompty", prompty);
+            return prompty.ChatResponseMessage.Content;
+        }
+
+        [HttpGet(Name = "GetChatRequest")]
+        public string Get()
+        {
+            //var inputs = new Dictionary<string, dynamic>
+            //{
+            //    { "firstName", firstName },
+            //    { "lastName", lastName },
+            //    { "question", question }
+            //};
+            // load chat.json file as new dictionary<string, string>
+            var jsonInputs = System.IO.File.ReadAllText("chat.json");
+            // convert json to dictionary<string, string>
+            var inputs = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonInputs);
+            string result = RunPrompt(inputs).Result;
+            Console.WriteLine(result);
+            return result;
+        }
+>>>>>>> 675001a (chat updates with static chat.json data)
     }
 
 
