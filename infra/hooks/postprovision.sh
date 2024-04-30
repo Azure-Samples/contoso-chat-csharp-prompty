4f36fe3550cabfcab05b6966052cd10ac7aa633d
welasco <26201510+Welasco@users.noreply.github.com>
2024-04-30 12:15:56 -0500

    Fixed Hook Linux version with rith registry name

--- infra/hooks/postprovision.sh
+++ infra/hooks/postprovision.sh
@@ -2,6 +2,6 @@
 
 echo  "Building contosochatapi:latest..."
 az acr build --subscription ${AZURE_SUBSCRIPTION_ID} --registry ${AZURE_CONTAINER_REGISTRY_NAME} --image contosochatapi:latest ./src/ContosoChatAPI/
-image_name="${AZURE_REGISTRY_NAME}.azurecr.io/contosochatapi:latest"
+image_name="${AZURE_CONTAINER_REGISTRY_NAME}.azurecr.io/contosochatapi:latest"
 az containerapp update --subscription ${AZURE_SUBSCRIPTION_ID} --name ${SERVICE_ACA_NAME} --resource-group ${RESOURCE_GROUP_NAME} --image ${image_name}
 az containerapp ingress update --name ${SERVICE_ACA_NAME} --resource-group ${RESOURCE_GROUP_NAME} --target-port 8080
