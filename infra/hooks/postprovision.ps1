15e89c8b356b5a37a82d03655c218895b869a895
Kristen Womack <5034778+kristenwomack@users.noreply.github.com>
2024-05-15 14:03:26 +0000

    Add `azd` service, remove custom `az` deployment scripts, update metadata name to match repo name

--- infra/hooks/postprovision.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-
-Write-Output  "Building contosochatapi:latest..."
-az acr build --subscription $env:AZURE_SUBSCRIPTION_ID --registry $env:AZURE_CONTAINER_REGISTRY_NAME --image contosochatapi:latest ./src/ContosoChatAPI/ContosoChatAPI/
-$image_name = $env:AZURE_CONTAINER_REGISTRY_NAME + '.azurecr.io/contosochatapi:latest'
-az containerapp update --subscription $env:AZURE_SUBSCRIPTION_ID --name $env:SERVICE_ACA_NAME --resource-group $env:RESOURCE_GROUP_NAME --image $image_name
-az containerapp ingress update --subscription $env:AZURE_SUBSCRIPTION_ID --name $env:SERVICE_ACA_NAME --resource-group $env:RESOURCE_GROUP_NAME --target-port 8080
--- infra/hooks/postprovision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo  "Building contosochatapi:latest..."
-az acr build --subscription ${AZURE_SUBSCRIPTION_ID} --registry ${AZURE_CONTAINER_REGISTRY_NAME} --image contosochatapi:latest ./src/ContosoChatAPI/ContosoChatAPI
-image_name="${AZURE_CONTAINER_REGISTRY_NAME}.azurecr.io/contosochatapi:latest"
-az containerapp update --subscription ${AZURE_SUBSCRIPTION_ID} --name ${SERVICE_ACA_NAME} --resource-group ${RESOURCE_GROUP_NAME} --image ${image_name}
-az containerapp ingress update --subscription ${AZURE_SUBSCRIPTION_ID} --name ${SERVICE_ACA_NAME} --resource-group ${RESOURCE_GROUP_NAME} --target-port 8080
