5e3ef27a1a38589cd4c4371156a5002529761634
Natalia Venditto <nataliafrontend@gmail.com>
2024-05-17 14:40:10 +0000

    fix permissions in script run

--- infra/hooks/postprovision.sh
+++ infra/hooks/postprovision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo  "Building contosochatapi:latest..."
+echo  "#### Building contosochatapi:latest... ####"
+
 az acr build --subscription ${AZURE_SUBSCRIPTION_ID} --registry ${AZURE_CONTAINER_REGISTRY_NAME} --image contosochatapi:latest ./src/ContosoChatAPI/ContosoChatAPI
 image_name="${AZURE_CONTAINER_REGISTRY_NAME}.azurecr.io/contosochatapi:latest"
