eaa5f2bbb652c7ed708bd8deca381394bfa9bb45
Kristen Womack <5034778+kristenwomack@users.noreply.github.com>
2024-05-17 03:39:27 +0000

    updates to services and the postprovision scripts from the PR feedback

---
+++ infra/hooks/postprovision.ps1
@@ -0,0 +1,5 @@
+#!/usr/bin/env pwsh
+
+Write-Output  "Building contosochatapi:latest..."
+az acr build --subscription $env:AZURE_SUBSCRIPTION_ID --registry $env:AZURE_CONTAINER_REGISTRY_NAME --image contosochatapi:latest ./src/ContosoChatAPI/ContosoChatAPI/
+$image_name = $env:AZURE_CONTAINER_REGISTRY_NAME + '.azurecr.io/contosochatapi:latest'
