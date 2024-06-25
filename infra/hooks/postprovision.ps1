1ce954eba1f8c8797f5f0e969c8f3b18f18077b7
Kristen Womack <5034778+kristenwomack@users.noreply.github.com>
2024-06-25 13:54:20 +0000

    remove scripts in favor of azd services

--- infra/hooks/postprovision.ps1
+++ infra/hooks/postprovision.ps1
@@ -3,4 +3,3 @@
 Write-Output  "#### Building contosochatapi:latest...####"
 
 az acr build --subscription $env:AZURE_SUBSCRIPTION_ID --registry $env:AZURE_CONTAINER_REGISTRY_NAME --image contosochatapi:latest ./src/ContosoChatAPI/ContosoChatAPI/
-$image_name = $env:AZURE_CONTAINER_REGISTRY_NAME + '.azurecr.io/contosochatapi:latest'
