5810bf8bc5336763f018929c0621d90f688c2779
Wallace Breza <wallace.breza@microsoft.com>
2024-07-26 12:19:34 -0700

    Minor fixes

--- infra/hooks/postprovision.ps1
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env pwsh
-
-Write-Output  "#### Building contosochatapi:latest...####"
-
-az acr build --subscription $env:AZURE_SUBSCRIPTION_ID --registry $env:AZURE_CONTAINER_REGISTRY_NAME --image contosochatapi:latest ./src/ContosoChatAPI/ContosoChatAPI/
